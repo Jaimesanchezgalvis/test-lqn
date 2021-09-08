50892836b0b052d391b8ae8d96f743bbcf543809
jaime sanchez galvis <jaimesanchezgalvis@gmail.com>
2021-09-08 16:32:38 -0500

    clean

--- Parte 2/src/router/AppRouter.js
+++ Parte 2/src/router/AppRouter.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Navbar } from '../ui/Navbar'
 import {
-    BrowserRouter as Router,
+    HashRouter  as Router,
     Switch,
     Route,
 } from "react-router-dom";
